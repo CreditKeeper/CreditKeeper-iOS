9174a7e9568f9ad3d816208a94b511855bb34fc1
Nick Molargik <nmolargik@gmail.com>
2023-09-05 21:54:25 -0400

    spice up the review view

--- CreditKeeper/ChildViews/ReviewView.swift
+++ CreditKeeper/ChildViews/ReviewView.swift
@@ -11,22 +11,45 @@ struct ReviewView: View {
     @ObservedObject var viewModel : MainViewModel
     @Binding var ride: Ride
     @Binding var showReview : Bool
+    @State var showThanks = false
     @State private var rating: Int = 0
     
     var body: some View {
         HStack {
-            ForEach(1..<6) { index in
-                Image(systemName: index <= rating ? "star.fill" : "star")
-                    .resizable()
-                    .frame(width: 30, height: 30)
-                    .foregroundStyle(index <= rating ? Color("uiCapsuleYellow") : .gray)
-                    .onTapGesture {
-                        rating = index
-                        playHaptic()
-                        // submit ride rating
-                        
-                        showReview = false
-                    }
+            if (showThanks) {
+                Text("Thank you!")
+                    .bold()
+                    .foregroundStyle(.white)
+                    .font(.title2)
+            } else {
+                ForEach(1..<6) { index in
+                    Image(systemName: index <= rating ? "star.fill" : "star")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundStyle(index <= rating ? Color("uiCapsuleYellow") : .white)
+                        .onTapGesture {
+                            withAnimation {
+                                rating = index
+                                playHaptic()
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                    withAnimation {
+                                        showThanks = true
+                                    }
+                                    
+                                    
+                                    // submit ride rating
+                                    
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+                                        withAnimation {
+                                            showThanks = false
+                                            showReview = false
+                                        }
+                                        
+                                    }
+                                }
+                            }
+                        }
+                }
             }
         }
     }
