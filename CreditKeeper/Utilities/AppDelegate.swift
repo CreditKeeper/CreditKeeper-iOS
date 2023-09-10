e9088550c8f855befe6e6d69d1e348d8823f9dc1
Nythepegasus <nythepegasus84@gmail.com>
2023-09-10 19:08:12 -0400

    Begin work on local SQLite in-app

--- CreditKeeper/Utilities/AppDelegate.swift
+++ CreditKeeper/Utilities/AppDelegate.swift
@@ -11,6 +11,11 @@ import Combine
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Getting Parks")
+        LocalDBController.shared.get_parks()
+        print("Getting Rides")
+        LocalDBController.shared.get_rides()
+        
         FirebaseApp.configure()
         
         UITableView.appearance().backgroundColor = UIColor.clear
